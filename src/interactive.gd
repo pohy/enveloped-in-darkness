30d2c62541be26ea8da82552dbd0ea2b4306e177
pohy <0ronon0@gmail.com>
2025-02-22 20:30:15 +0100

    Configure build

---
+++ src/interactive.gd
@@ -0,0 +1,15 @@
+class_name Interactive
+extends CollisionObject3D
+
+enum Name {
+	DOOR,
+}
+
+signal interacted(name: Interactive.Name)
+
+@export var interactive_name: Interactive.Name = Interactive.Name.DOOR
+
+
+func interact() -> void:
+	interacted.emit(interactive_name)
+	print("Interacted with %s (%s)" % [name, interactive_name])
