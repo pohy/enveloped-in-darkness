96feec13ac2cf8415ead2c1082922e0847b94618
pohy <0ronon0@gmail.com>
2025-02-22 16:31:58 +0100

    Add custom label parsing

--- src/dialogue_control.gd
+++ src/dialogue_control.gd
@@ -6,21 +6,21 @@ signal last_line_reached
 @export var dialogue: DialogueResource
 @export var dialogue_label: RichTextLabel
 
-var _current_line: int = 0:
+var _current_line_idx: int = 0:
 	set(value):
-		_current_line = value
+		_current_line_idx = value
 		if dialogue_label is RichTextLabel:
-			dialogue_label.text = dialogue.lines[_current_line]
+			dialogue_label.text = dialogue.lines[_current_line_idx]
 
 
 func advance() -> void:
-	var next_line = _current_line + 1
+	var next_line = _current_line_idx + 1
 	if next_line > dialogue.lines.size() - 1:
 		printerr("End of dialogue")
 		return
 	else:
 		last_line_reached.emit()
-		_current_line = next_line
+		_current_line_idx = next_line
 
 
 func _ready() -> void:
@@ -29,4 +29,26 @@ func _ready() -> void:
 
 	dialogue.load()
 
-	dialogue_label.text = dialogue.lines[_current_line]
+	dialogue_label.text = dialogue.lines[_current_line_idx]
+
+	return
+	# TODO: F U J K Y
+	_current_line_idx = 3
+
+	var line := dialogue.lines[_current_line_idx]
+
+	var player_input_parsed = dialogue.get_player_input_from_line(line)
+	if player_input_parsed:
+		print("Player player_input_parsed found: %s" % player_input_parsed)
+		dialogue_label.text = player_input_parsed["stripped"]
+	else:
+		print("No player player_input_parsed found")
+
+	var continue_parsed = dialogue.get_continue_from_line(
+		player_input_parsed["stripped"] if player_input_parsed else line
+	)
+	if continue_parsed:
+		print("Continue found: %s" % continue_parsed)
+		dialogue_label.text = continue_parsed["stripped"]
+	else:
+		print("No continue found")
--- src/dialogue_resource.gd
+++ src/dialogue_resource.gd
@@ -24,3 +24,50 @@ func load() -> void:
 		printerr("No lines in file: " + file_path)
 
 	file.close()
+
+
+func get_player_input_from_line(line: String):
+	var player_input_tag := "player_input"
+	var name_attr = "name"
+
+	var start_partial := "[%s %s=" % [player_input_tag, name_attr]
+	var start = line.find(start_partial)
+	if start == -1:
+		return {}
+	var start_closing := line.find("]")
+	if start_closing == -1:
+		printerr("Mising closing bracket for player_input in line:\n%s" % line)
+		return null
+
+	var end_partial := "[/%s]" % player_input_tag
+	var end = line.find(end_partial)
+	if end == -1:
+		printerr("Mising closing tag for player_input in line:\n%s" % line)
+		return null
+
+	var input_name := line.substr(
+		start + len(start_partial), start_closing - start - len(start_partial)
+	)
+
+	var match := line.substr(start, end + len(end_partial) - start)
+
+	return {"name_attr": input_name, "stripped": line.replace(match, "")}
+
+
+func get_continue_from_line(line: String):
+	var continue_tag := "continue"
+
+	var start_partial := "[%s]" % continue_tag
+	var start = line.find(start_partial)
+	if start == -1:
+		return null
+
+	var end_partial := "[/%s]" % continue_tag
+	var end = line.find(end_partial)
+	if end == -1:
+		printerr("Mising closing tag for continue in line:\n%s" % line)
+		return null
+
+	var match := line.substr(start, end + len(end_partial) - start)
+
+	return {"stripped": line.replace(match, "")}
