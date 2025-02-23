ee8370e84bf12673ee35344dad55ae84cdac4b52
pohy <0ronon0@gmail.com>
2025-02-23 15:43:52 +0100

    Add enable/disable to interactive

--- src/interactive.gd
+++ src/interactive.gd
@@ -8,8 +8,20 @@ enum Name {
 signal interacted(name: Interactive.Name)
 
 @export var interactive_name: Interactive.Name = Interactive.Name.DOOR
+@export var is_enabled := true
 
 
 func interact() -> void:
+	if not is_enabled:
+		return
+
 	interacted.emit(interactive_name)
 	print("Interacted with %s (%s)" % [name, interactive_name])
+
+
+func disable() -> void:
+	is_enabled = false
+
+
+func enable() -> void:
+	is_enabled = true
